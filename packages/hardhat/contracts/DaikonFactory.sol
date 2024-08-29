f210711641bf55f8c2184870babc4fa8328938ab
mart1n <20109376+mart1n-xyz@users.noreply.github.com>
2024-08-29 17:29:21 +0200

    change from factory to registry

--- packages/hardhat/contracts/DaikonFactory.sol
+++ packages/hardhat/contracts/DaikonFactory.sol
@@ -6,32 +6,90 @@ import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Daikon is Ownable {
-    constructor(string memory _initialName) Ownable() {
-        name = _initialName;
-    }
-
-    string public name;
-
-    function setName(string memory _newName) public onlyOwner {
-        name = _newName;
-    }
-}
-
 contract DaikonFactory is Ownable {
-    event DaikonDeployed(address indexed daikonAddress, address indexed owner, string name);
+    event DaikonCreated(uint256 indexed daikonId, address indexed owner, string name);
+    event DaikonNameChanged(uint256 indexed daikonId, string newName);
+
+    struct Daikon {
+        uint256 id;
+        address owner;
+        string name;
+    }
+
+    Daikon[] public daikons;
+    mapping(address => uint256[]) public ownerToDaikonIds;
 
     constructor() Ownable() {}
 
     /**
-     * Deploy a new Daikon contract
+     * Create a new Daikon within the registry
      * @param _name The initial name for the Daikon
      */
-    function deployDaikon(string memory _name) public returns (address) {
-        Daikon newDaikon = new Daikon(_name);
-        newDaikon.transferOwnership(msg.sender);
-        emit DaikonDeployed(address(newDaikon), msg.sender, _name);
-        console.log("New Daikon deployed at:", address(newDaikon));
-        return address(newDaikon);
+    function createDaikon(string memory _name) public returns (uint256) {
+        uint256 newDaikonId = daikons.length;
+        daikons.push(Daikon(newDaikonId, msg.sender, _name));
+        ownerToDaikonIds[msg.sender].push(newDaikonId);
+        emit DaikonCreated(newDaikonId, msg.sender, _name);
+        console.log("New Daikon created with ID:", newDaikonId);
+        return newDaikonId;
     }
+
+    /**
+     * Get the number of Daikons in the registry
+     */
+    function getDaikonCount() public view returns (uint256) {
+        return daikons.length;
+    }
+
+    /**
+     * Get Daikon info by ID
+     */
+    function getDaikon(uint256 _daikonId) public view returns (Daikon memory) {
+        require(_daikonId < daikons.length, "Daikon does not exist");
+        return daikons[_daikonId];
+    }
+
+    /**
+     * Get Daikons owned by an address
+     */
+    function getDaikonsByOwner(address _owner) public view returns (uint256[] memory) {
+        return ownerToDaikonIds[_owner];
+    }
+
+    /**
+     * Set name for a Daikon
+     */
+    function setDaikonName(uint256 _daikonId, string memory _newName) public {
+        require(_daikonId < daikons.length, "Daikon does not exist");
+        require(daikons[_daikonId].owner == msg.sender, "Only Daikon owner can set name");
+        daikons[_daikonId].name = _newName;
+        emit DaikonNameChanged(_daikonId, _newName);
+    }
+
+    /**
+     * Transfer ownership of a Daikon
+     */
+    function transferDaikonOwnership(uint256 _daikonId, address _newOwner) public {
+        require(_daikonId < daikons.length, "Daikon does not exist");
+        require(daikons[_daikonId].owner == msg.sender, "Only Daikon owner can transfer ownership");
+        require(_newOwner != address(0), "New owner cannot be zero address");
+
+        // Remove Daikon from current owner's list
+        uint256[] storage currentOwnerDaikons = ownerToDaikonIds[msg.sender];
+        for (uint i = 0; i < currentOwnerDaikons.length; i++) {
+            if (currentOwnerDaikons[i] == _daikonId) {
+                currentOwnerDaikons[i] = currentOwnerDaikons[currentOwnerDaikons.length - 1];
+                currentOwnerDaikons.pop();
+                break;
+            }
+        }
+
+        // Add Daikon to new owner's list
+        ownerToDaikonIds[_newOwner].push(_daikonId);
+
+        // Update Daikon owner
+        daikons[_daikonId].owner = _newOwner;
+    }
+
+
 }
--- packages/nextjs/contracts/deployedContracts.ts
+++ packages/nextjs/contracts/deployedContracts.ts
@@ -7,8 +7,57 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 const deployedContracts = {
   31337: {
     DaikonFactory: {
-      address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+      address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
       abi: [
+        {
+          inputs: [],
+          stateMutability: "nonpayable",
+          type: "constructor",
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: "uint256",
+              name: "daikonId",
+              type: "uint256",
+            },
+            {
+              indexed: true,
+              internalType: "address",
+              name: "owner",
+              type: "address",
+            },
+            {
+              indexed: false,
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          name: "DaikonCreated",
+          type: "event",
+        },
+        {
+          anonymous: false,
+          inputs: [
+            {
+              indexed: true,
+              internalType: "uint256",
+              name: "daikonId",
+              type: "uint256",
+            },
+            {
+              indexed: false,
+              internalType: "string",
+              name: "newName",
+              type: "string",
+            },
+          ],
+          name: "DaikonNameChanged",
+          type: "event",
+        },
         {
           anonymous: false,
           inputs: [
@@ -29,6 +78,122 @@ const deployedContracts = {
           type: "event",
         },
         {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_name",
+              type: "string",
+            },
+          ],
+          name: "createDaikon",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "daikons",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "id",
+              type: "uint256",
+            },
+            {
+              internalType: "address",
+              name: "owner",
+              type: "address",
+            },
+            {
+              internalType: "string",
+              name: "name",
+              type: "string",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_daikonId",
+              type: "uint256",
+            },
+          ],
+          name: "getDaikon",
+          outputs: [
+            {
+              components: [
+                {
+                  internalType: "uint256",
+                  name: "id",
+                  type: "uint256",
+                },
+                {
+                  internalType: "address",
+                  name: "owner",
+                  type: "address",
+                },
+                {
+                  internalType: "string",
+                  name: "name",
+                  type: "string",
+                },
+              ],
+              internalType: "struct DaikonFactory.Daikon",
+              name: "",
+              type: "tuple",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [],
+          name: "getDaikonCount",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "address",
+              name: "_owner",
+              type: "address",
+            },
+          ],
+          name: "getDaikonsByOwner",
+          outputs: [
+            {
+              internalType: "uint256[]",
+              name: "",
+              type: "uint256[]",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
           inputs: [],
           name: "owner",
           outputs: [
@@ -42,8 +207,68 @@ const deployedContracts = {
           type: "function",
         },
         {
+          inputs: [
+            {
+              internalType: "address",
+              name: "",
+              type: "address",
+            },
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          name: "ownerToDaikonIds",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "view",
+          type: "function",
+        },
+        {
           inputs: [],
           name: "renounceOwnership",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_daikonId",
+              type: "uint256",
+            },
+            {
+              internalType: "string",
+              name: "_newName",
+              type: "string",
+            },
+          ],
+          name: "setDaikonName",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+        {
+          inputs: [
+            {
+              internalType: "uint256",
+              name: "_daikonId",
+              type: "uint256",
+            },
+            {
+              internalType: "address",
+              name: "_newOwner",
+              type: "address",
+            },
+          ],
+          name: "transferDaikonOwnership",
           outputs: [],
           stateMutability: "nonpayable",
           type: "function",
@@ -60,10 +285,6 @@ const deployedContracts = {
           outputs: [],
           stateMutability: "nonpayable",
           type: "function",
-        },
-        {
-          stateMutability: "payable",
-          type: "receive",
         },
       ],
       inheritedFunctions: {
